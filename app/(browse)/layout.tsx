8c4c0e500709afd9ac8641387e93c5e2e38fbc72
Rodrigo Bonatti <bonatti@Rodrigos-MacBook-Pro.local>
2023-12-18 00:15:44 -0300

    Create sidebar

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,4 +1,6 @@
 import { Navbar } from './_components/navbar'
+import { Sidebar } from './_components/sidebar'
+import { Container } from './_components/container'
 
 export default function BrowseLayout({
   children
@@ -9,7 +11,11 @@ export default function BrowseLayout({
     <>
       <Navbar />
 
-      <div className="flex h-full pt-20">{children}</div>
+      <div className="flex h-full pt-20">
+        <Sidebar />
+
+        <Container>{children}</Container>
+      </div>
     </>
   )
 }
