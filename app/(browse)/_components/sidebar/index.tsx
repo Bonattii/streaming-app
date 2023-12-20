faf6f399469a34e25f9c51a5aa145cb17b7336c4
Rodrigo Bonatti <bonatti@Rodrigos-MacBook-Pro.local>
2023-12-20 10:38:37 -0300

    Create follow list

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,17 +1,21 @@
+import { getFollowedUsers } from '@/lib/follow-service'
 import { getRecommended } from '@/lib/recommended-service'
 
 import { Wrapper } from './wrapper'
 import { Toggle, ToggleSkeleton } from './toggle'
+import { Following, FollowingSkeleton } from './following'
 import { Recommended, RecommendedSkeleton } from './recommended'
 
 export async function Sidebar() {
   const recommended = await getRecommended()
+  const following = await getFollowedUsers()
 
   return (
     <Wrapper>
       <Toggle />
 
       <div className="space-y-4 pt-4 lg:pt-0">
+        <Following data={following} />
         <Recommended data={recommended} />
       </div>
     </Wrapper>
@@ -22,6 +26,7 @@ export function SidebarSkeleton() {
   return (
     <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2d2e35] z-50">
       <ToggleSkeleton />
+      <FollowingSkeleton />
       <RecommendedSkeleton />
     </aside>
   )
