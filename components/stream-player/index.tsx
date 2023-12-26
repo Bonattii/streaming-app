87893411b2ee8281365d97e88d961a8187ac2fbd
Rodrigo Bonatti <bonatti@Rodrigos-MacBook-Pro.local>
2023-12-26 15:38:47 -0300

    Create stream header

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -7,6 +7,7 @@ import { cn } from '@/lib/utils'
 import { useChatSidebar } from '@/store/use-chat-sidebar'
 import { useViewerToken } from '@/hooks/use-viewer-token'
 
+import { Header, HeaderSkeleton } from './header'
 import { ChatToggle } from './chat-toggle'
 import { Chat, ChatSkeleton } from './chat'
 import { Video, VideoSkeleton } from './video'
@@ -44,6 +45,15 @@ export function StreamPlayer({ stream, user, isFollowing }: StreamPlayerProps) {
       >
         <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
           <Video hostName={user.username} hostIdentity={user.id} />
+
+          <Header
+            hostName={user.username}
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            imageUrl={user.imageUrl}
+            isFollowing={isFollowing}
+            name={stream.name}
+          />
         </div>
 
         <div className={cn('col-span-1', collapsed && 'hidden')}>
@@ -64,10 +74,11 @@ export function StreamPlayer({ stream, user, isFollowing }: StreamPlayerProps) {
 
 export function StreamPlayerSkeleton() {
   return (
-    <div className="gtid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
+    <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
       <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
         <VideoSkeleton />
-        {/* TODO: Header Skeleton */}
+
+        <HeaderSkeleton />
       </div>
 
       <div className="col-span-1 bg-background">
