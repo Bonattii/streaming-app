360c8b30668ef7b0b3ec99425d17eebaf821cd29
Rodrigo Bonatti <bonatti@Rodrigos-MacBook-Pro.local>
2024-01-10 17:15:28 -0300

    Create search page

---
+++ lib/search-service.ts
@@ -0,0 +1,88 @@
+import { db } from '@/lib/db'
+import { getSelf } from '@/lib/auth-service'
+
+export const getSearch = async (term?: string) => {
+  let userId
+  let streams = []
+
+  try {
+    const self = await getSelf()
+    userId = self.id
+  } catch {
+    userId = null
+  }
+
+  if (userId) {
+    // Don't show blocked users in search results
+    streams = await db.stream.findMany({
+      where: {
+        user: {
+          NOT: {
+            blocking: {
+              some: {
+                blockedId: userId
+              }
+            }
+          }
+        },
+        OR: [
+          {
+            name: {
+              contains: term
+            }
+          },
+          {
+            user: {
+              username: {
+                contains: term
+              }
+            }
+          }
+        ]
+      },
+      include: {
+        user: true
+      },
+      orderBy: [
+        {
+          isLive: 'desc'
+        },
+        {
+          updatedAt: 'desc'
+        }
+      ]
+    })
+  } else {
+    streams = await db.stream.findMany({
+      where: {
+        OR: [
+          {
+            name: {
+              contains: term
+            }
+          },
+          {
+            user: {
+              username: {
+                contains: term
+              }
+            }
+          }
+        ]
+      },
+      include: {
+        user: true
+      },
+      orderBy: [
+        {
+          isLive: 'desc'
+        },
+        {
+          updatedAt: 'desc'
+        }
+      ]
+    })
+  }
+
+  return streams
+}
